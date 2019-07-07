644c79821885a2ee5177e17e32225abe648a67ba
csergey8 <c.sergey@protonmail.com>
2019-07-07 12:52:23 +0300

    two tasks done

---
+++ FL11_HW6/homework/js/task1.js
@@ -0,0 +1,18 @@
+const a1 = parseFloat(prompt('enter the coordinate value along the x-axis of point a(a1):'));
+const a2 = parseFloat(prompt('enter the coordinate value along the y-axis of point a(a2):'));
+const b1 = parseFloat(prompt('enter the coordinate value along the x-axis of point b(b1):'));
+const b2 = parseFloat(prompt('enter the coordinate value along the y-axis of point b(b2):'));
+const c1 = parseFloat(prompt('enter the coordinate value along the x-axis of point c(c1):'));
+const c2 = parseFloat(prompt('enter the coordinate value along the y-axis of point c(c2):'));
+const divider = 2;
+const midpointX = (a1 + b1) / divider;
+const midpointY = (a2 + b2) / divider;
+let result;
+
+if(midpointX === c1 && midpointY === c2){
+  result = true;
+} else {
+  result = false;
+}
+
+console.log(result);
---
+++ FL11_HW6/homework/js/task2.js
@@ -0,0 +1,21 @@
+const a = parseFloat(prompt('Enter the length of the side A of the triangle'));
+const b = parseFloat(prompt('Enter the length of the side B of the triangle'));
+const c = parseFloat(prompt('Enter the length of the side C of the triangle'));
+let result;
+
+if (a >= 0 && b >= 0 && c >= 0) {
+  if (a < b + c && b < a + c && c < a + b) {
+    if (a === b && a === c && b === c) {
+      result = 'Eequivalent triangle';
+    } else if (a !== b && a !== c && b !== c) {
+      result = 'Normal triangle';
+    } else {
+      result = 'Isosceles triangle';
+    }
+  } else {
+    result = 'Triangle doesn’t exist';
+  }
+} else {
+  result = 'Length of side must be grater than 0';
+}
+console.log(result);
